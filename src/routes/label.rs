0014f7aa504ac186144d4314ad394b6175c92954
Daniël Terwiel <daniel@terwiel.io>
2023-07-19 18:49:39 +0200

    label child

--- src/routes/label.rs
+++ src/routes/label.rs
@@ -1,8 +1,27 @@
 use leptos::*;
 
+// TODO: see if you can validate Children, being a String or Input.
+// Maybe use: https://docs.rs/leptos/latest/leptos/struct.NodeRef.html
+#[component]
+fn Label(cx: Scope, children: Children) -> impl IntoView {
+    view! { cx,
+        <label>
+            {children(cx)}
+        </label>
+    }
+}
+
 #[component]
 pub fn LabelPage(cx: Scope) -> impl IntoView {
     view! { cx,
         <h1>"Label"</h1>
+        <h2>"Example"</h2>
+        <Label>"First name"</Label>
+        <input
+            id="first-name"
+            class=("p-1 border border-slate-500")
+        />
+
     }
 }
+
