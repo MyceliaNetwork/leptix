9e7b1b1fd9eba02d22102b90be1b68b464e976cb
Daniël Terwiel <daniel@terwiel.io>
2023-07-19 19:53:01 +0200

    for attribute!

--- src/routes/label.rs
+++ src/routes/label.rs
@@ -3,9 +3,13 @@ use leptos::*;
 // TODO: see if you can validate Children, being a String or Input.
 // Maybe use: https://docs.rs/leptos/latest/leptos/struct.NodeRef.html
 #[component]
-fn Label(cx: Scope, children: Children) -> impl IntoView {
+fn Label(
+    cx: Scope,
+    #[prop(into)] html_for: String,
+    children: Children
+) -> impl IntoView {
     view! { cx,
-        <label>
+        <label for=html_for>
             {children(cx)}
         </label>
     }
@@ -16,7 +20,7 @@ pub fn LabelPage(cx: Scope) -> impl IntoView {
     view! { cx,
         <h1>"Label"</h1>
         <h2>"Example"</h2>
-        <Label>"First name"</Label>
+        <Label html_for="first-name">"First name"</Label>
         <input
             id="first-name"
             class=("p-1 border border-slate-500")
