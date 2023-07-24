01e62740402e835b88a3cf6630b44a4f72f4ad88
Daniël Terwiel <daniel@terwiel.io>
2023-07-24 13:39:50 +0200

    feat[checkbox]: required attribute

--- src/routes/checkbox.rs
+++ src/routes/checkbox.rs
@@ -7,27 +7,38 @@ fn Checkbox (
     #[prop(default = None, optional, into)]
     class: Option<AttributeValue>,
 
-    // #[prop(into)]
-    // disabled: ReadSignal<bool>,
     #[prop(optional, into)]
     disabled: Option<AttributeValue>,
+
+    #[prop(optional, into)]
+    required: Option<AttributeValue>,
 
     #[prop(into)]
     checked: ReadSignal<bool>,
     
 ) -> impl IntoView {
+    // disabled
     let mut disabled = disabled;
     if let None = disabled {
         disabled = Some(Box::new(Attribute::Bool(false)))
     }
-
     let disabled = disabled.unwrap();
     let disabled = disabled.into_attribute_boxed(cx);
+
+    // required
+    let mut required = required;
+    if let None = required {
+        required = Some(Box::new(Attribute::Bool(false)))
+    }
+    let required = required.unwrap();
+    let required = required.into_attribute_boxed(cx);
+ 
     view! { cx,
         <input
             type="checkbox"
             class=class
             disabled=disabled
+            required=required
             prop:value=checked
         />
     }
@@ -37,22 +48,38 @@ fn Checkbox (
 pub fn CheckboxPage(cx: Scope) -> impl IntoView {
     let (checked, set_checked) = create_signal(cx, true);
     let (disabled, set_disabled) = create_signal(cx, false);
+    let (required, set_required) = create_signal(cx, false);
 
     view! { cx,
         <h1>"Checkbox"</h1>
         <h2>"Example"</h2>
-        <button
-            type="button"
-            on:click=move |_| {
-                set_disabled(!disabled.get())
-            }
-        >
-            "Toggle disabled"
-        </button>
+        
+        <div>
+            <button
+                type="button"
+                on:click=move |_| {
+                    set_disabled(!disabled.get())
+                }
+            >
+                "Toggle disabled"
+            </button>
+        </div>
 
-        <Checkbox
+        <div>
+            <button
+                type="button"
+                on:click=move |_| {
+                    set_required(!required.get())
+                }
+            >
+                "Toggle required"
+            </button>
+        </div>
+
+    <Checkbox
             checked=checked
             disabled=disabled
+            required=required
             on:change=move |ev| {
                 set_checked(event_target_checked(&ev));
             }
